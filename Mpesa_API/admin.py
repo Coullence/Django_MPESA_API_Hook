b31d6d80c87756c91e50f258ea8c418780793cba
devop <devop@localhost.localdomain>
2021-06-18 08:25:17 +0300

    Added callback shiftdssswws

--- Mpesa_API/admin.py
+++ Mpesa_API/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import MpesaPayment
+from .models import MpesaPayment, STKPayment
 
 
 admin.site.register(MpesaPayment)
+admin.site.register(STKPayment)
