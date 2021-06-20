c7bb6b4aed92c14858efb033d21eb9117003220b
devop <devop@localhost.localdomain>
2021-06-21 02:32:48 +0300

    Updated

--- Mpesa_API/models.py
+++ Mpesa_API/models.py
@@ -52,7 +52,6 @@ class MpesaPayment(BaseModel):
         return self.first_name
     
 class STKPayment(BaseModel):
-<<<<<<< HEAD
     MerchantRequestID = models.TextField(max_length=100)
     CheckoutRequestID = models.TextField(max_length=100)
     ResultDesc = models.TextField(max_length=100)      
@@ -60,11 +59,6 @@ class STKPayment(BaseModel):
     transaction_code = models.TextField(max_length=100)
     TransactionDate = models.DateTimeField(max_length=100)    
     phone_number = models.TextField(max_length=20)    
-=======
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    transaction_code = models.TextField()
-    phone_number = models.TextField()    
->>>>>>> 17c0a8f0b85077de1abffd6861f1c3a8295c6a91
     
 
     class Meta:
@@ -72,8 +66,4 @@ class STKPayment(BaseModel):
         verbose_name_plural = 'stk Payments'
 
     def __str__(self):
-<<<<<<< HEAD
         return self.phone_number
-=======
-        return self.amount
->>>>>>> 17c0a8f0b85077de1abffd6861f1c3a8295c6a91
